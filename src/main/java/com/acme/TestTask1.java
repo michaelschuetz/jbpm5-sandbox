9ea769fd9ca6941a010f5375c70ac2e141082f6e
Michael Schuetz <michaelschuetz83@gmail.com>
2011-06-22 09:23:05 +0200

    setup sample jbpm5 process by appr. plugin

---
+++ src/main/java/com/acme/TestTask1.java
@@ -0,0 +1,8 @@
+package com.acme;
+public class TestTask1 {
+  private static final String MARKER="------------------------";
+  private static final String TASK_NAME="task1";
+  public void doTask(){
+    System.out.println(MARKER + " " + TASK_NAME+ " "+ MARKER);
+  }
+}
